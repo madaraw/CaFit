682e07f314faf858b7eb6a0c2988d4fca7642e47
madaraw <rachediwalid@yahoo.fr>
2025-03-11 12:51:49 +0100

    Added some text and style to the home page

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bg-surface-950 flex items-center min-h-screen px-6 py-20 md:px-12 lg:px-20">
+    <div class="text-surface-100 text-center flex flex-col items-center gap-4">
+
+      <div class="text-primary font-bold text-lg leading-tight"><i
+          class="pi pi-discord !text-lg !leading-none" />&nbsp;POWERED BY MADARAW</div>
+      <div class="text-surface-0 font-bold text-4xl leading-tight">Your Fitness Journey Starts Here.</div>
+      <div class="text-surface-100 text-xl leading-normal">Build a personalized workout plan tailored to your goals and
+        preferences.</div>
+      <Button label="Create Workout" rounded class="mt-4" />
+    </div>
+  </div>
+</template>
+<script setup>
+  import Button from 'primevue/button';
+</script>
