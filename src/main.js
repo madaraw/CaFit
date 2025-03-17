e8fa926a10b7c06edee395fd1be7ad4693eb9b2f
madaraw <rachediwalid@yahoo.fr>
2025-03-17 18:44:07 +0100

    Add ConfirmationService to main.js for improved user feedback

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config';
 import Aura from '@primevue/themes/aura';
+import ConfirmationService from 'primevue/confirmationservice';
 
 const app = createApp(App)
 
@@ -15,5 +16,6 @@ app.use(PrimeVue, {
     }
 });
 app.use(router)
+app.use(ConfirmationService)
 
 app.mount('#app')
