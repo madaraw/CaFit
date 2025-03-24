ee5f46254c6226e48400c296aa78d9e317b6b621
madaraw <rachediwalid@yahoo.fr>
2025-03-24 02:29:08 +0100

    Enhance NavMenu component with improved branding and icon integration

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="bg-surface-950 p-4 flex justify-between items-center">
-        <div class="text-white text-lg font-bold">Cafit</div>
+        <div class="text-white text-2xl font-bold flex items-center gap-1">Cafit<i class="pi pi-mars"></i></div>
         <div class="flex space-x-4">
             <Button @click="$router.push({ name: 'home' })" label="Home" class="p-button-text text-white" />
             <Button @click="$router.push({ name: 'about' })" label="About" class="p-button-text text-white" />
